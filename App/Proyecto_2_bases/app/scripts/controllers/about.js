38732d6fd2f9d4e44652149b8e0facec93e0e26b
Bollack <bollackdtg@gmail.com>
2015-05-24 20:32:25 -0600

    Prueba carpeta generada por yeoman a Github.

---
+++ App/Proyecto_2_bases/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoOrnitocologiaAsociacionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectoOrnitocologiaAsociacionApp
+ */
+angular.module('proyectoOrnitocologiaAsociacionApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
