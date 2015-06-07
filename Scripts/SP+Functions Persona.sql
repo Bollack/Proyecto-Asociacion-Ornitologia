0baff1b8ef0f12f2e059497805f2f7876a482550
Bollack <bollackdtg@gmail.com>
2015-06-07 08:58:13 -0600

    Trabajo

---
+++ Scripts/SP+Functions Persona.sql
@@ -0,0 +1,72 @@
+Delimiter °°
+
+CREATE PROCEDURE Insert_Persona(IN pNombre VARCHAR(50), IN pApellido VARCHAR(60), IN pCorreo VARCHAR(100), IN pTelefono VARCHAR(20), 
+								IN fechaNacimiento DATE, IN pDireccion VARCHAR(100), In pSexo VARCHAR(40), IN tipoUser INT  
+                                IN pUsername VARCHAR(45), IN pPassword VARCHAR(100)) 
+	BEGIN
+		INSERT INTO persona (username, Password, Nombre, Apellido, Direccion,  Sexo, Fecha_Nacimiento, Tipo_Usuario
+    END
+
+
+
+°°
+Delimiter °°
+
+
+CREATE PROCEDURE AddTelToUser
+
+	BEGIN
+    
+    END
+
+°°
+
+Delimiter °°
+
+
+CREATE PROCEDURE AddCorreoToUser
+
+	BEGIN
+    
+    END
+
+°°
+Delimiter °°
+
+
+CREATE PROCEDURE DeleteTelFromUser
+	BEGIN
+    
+    END
+
+
+°°
+Delimiter °°
+
+CREATE PROCEDURE DeleteCorreoFromUser
+	BEGIN
+    
+    END
+
+
+°°
+Delimiter °°
+
+CREATE PROCEDURE UpdateCorreoFromUser
+	BEGIN
+    
+    END
+
+
+°°
+Delimiter °°
+
+CREATE PROCEDURE UpdateTelFromUser
+	BEGIN
+    
+    END
+
+°°
+Delimiter °°
+CREATE FUNCTION 
+
---
+++ Scripts/scripts creacion db/Segundo intento - Sábado 6 de Junio - desktop Daniel/Cambios-post creacion.sql
@@ -0,0 +1,64 @@
+#Cambios a hacer tras crear tablas y triggers del script de creacion
+
+ALTER TABLE `hidden_bird`.`especie` 
+DROP FOREIGN KEY `fk_Especie_Tamano1`,
+DROP FOREIGN KEY `fk_Especie_Tiempo_incubacion1`,
+DROP FOREIGN KEY `fk_Especie_Tipo_Nido1`,
+DROP FOREIGN KEY `fk_Especie_Tipo_iincubacion1`,
+DROP FOREIGN KEY `fk_Especie_ZonaVida1`,
+DROP FOREIGN KEY `fk_Especie_numero_huevos1`,
+DROP FOREIGN KEY `fk_Tipo_Ave_Forma_Pico1`,
+DROP FOREIGN KEY `fk_Tipo_Ave_Tipo_Huevos1`;
+ALTER TABLE `hidden_bird`.`especie` 
+CHANGE COLUMN `Forma_Pico_idForma_Pico` `Forma_Pico_idForma_Pico` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `Tipo_Huevos_idTipo_Huevos` `Tipo_Huevos_idTipo_Huevos` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `Tipo_iincubacion_idTipo_incubacion` `Tipo_iincubacion_idTipo_incubacion` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `numero_huevos_idnumero_huevos` `numero_huevos_idnumero_huevos` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `Tipo_Nido_idTipo_Nido` `Tipo_Nido_idTipo_Nido` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `Tiempo_incubacion_idTiempo_incubacion` `Tiempo_incubacion_idTiempo_incubacion` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `Tamano_idTamano` `Tamano_idTamano` INT(11) NULL DEFAULT '0' ,
+CHANGE COLUMN `ZonaVida_idZonaVida` `ZonaVida_idZonaVida` INT(11) NULL DEFAULT '0' ,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`idEspecie`),
+ADD UNIQUE INDEX `Nombre_cientifico_UNIQUE` (`Nombre_cientifico` ASC);
+ALTER TABLE `hidden_bird`.`especie` 
+ADD CONSTRAINT `fk_Especie_Tamano1`
+  FOREIGN KEY (`Tamano_idTamano`)
+  REFERENCES `hidden_bird`.`tamano` (`idTamano`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Especie_Tiempo_incubacion1`
+  FOREIGN KEY (`Tiempo_incubacion_idTiempo_incubacion`)
+  REFERENCES `hidden_bird`.`tiempo_incubacion` (`idTiempo_incubacion`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Especie_Tipo_Nido1`
+  FOREIGN KEY (`Tipo_Nido_idTipo_Nido`)
+  REFERENCES `hidden_bird`.`tipo_nido` (`idTipo_Nido`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Especie_Tipo_iincubacion1`
+  FOREIGN KEY (`Tipo_iincubacion_idTipo_incubacion`)
+  REFERENCES `hidden_bird`.`tipo_incubacion` (`idTipo_incubacion`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Especie_ZonaVida1`
+  FOREIGN KEY (`ZonaVida_idZonaVida`)
+  REFERENCES `hidden_bird`.`zonavida` (`idZonaVida`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Especie_numero_huevos1`
+  FOREIGN KEY (`numero_huevos_idnumero_huevos`)
+  REFERENCES `hidden_bird`.`cantidad_huevos` (`idcantidad_huevos`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Tipo_Ave_Forma_Pico1`
+  FOREIGN KEY (`Forma_Pico_idForma_Pico`)
+  REFERENCES `hidden_bird`.`forma_pico` (`idForma_Pico`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Tipo_Ave_Tipo_Huevos1`
+  FOREIGN KEY (`Tipo_Huevos_idTipo_Huevos`)
+  REFERENCES `hidden_bird`.`tipo_huevos` (`idTipo_Huevos`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
