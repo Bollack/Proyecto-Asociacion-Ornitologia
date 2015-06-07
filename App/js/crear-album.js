0b8d9996614b2b89f1d08974ea976eed2803ec8b
Cgironalas <cgironalas@gmail.com>
2015-06-06 19:10:05 -0600

    Perfil completo (con ejemplo)

--- App/js/crear-album.js
+++ App/js/crear-album.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 	var $counter = 2;
+	var $cConter = 2;
 	$('#agFoto').click(function(){
 		$('#fotos').append('<div class="form-group"><label for="Foto'+$counter+'">Foto '+$counter+':</label><input type="text" id="Foto'+$counter+'" class="form-control" placeholder="URL//"></div>');
 		$counter++;
 	});
+	$('#agColor').click(function()){
+		$('#agColor').append('<div class="form-group"><label for="Color'+$cCounter+'">Color '+$cCounter+':</label><select class="form-control" id="Color'+$cCounter+'"></select></div>');
+		$cCounter++;
+	}
 });
