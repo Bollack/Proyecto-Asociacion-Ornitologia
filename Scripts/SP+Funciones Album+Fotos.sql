f968319fbfdbd423552a1d3c532c0826480f513f
Bollack <bollackdtg@gmail.com>
2015-06-08 02:34:19 -0600

    RFERDFD

--- Scripts/SP+Funciones Album+Fotos.sql
+++ Scripts/SP+Funciones Album+Fotos.sql
@@ -8,18 +8,49 @@ CREATE FUNCTION getScientificNameBirdFromID(pID INT)
 RETURNS VARCHAR(100)
 	BEGIN
 		DECLARE name VARCHAR(100);
-        SELECT 
+        DECLARE CONTINUE HANDLER  FOR SQLEXCEPTION  SET name=NULL;
+        SELECT Nombre_cientifico
+        INTO name
+        FROM especie
+        WHERE idEspecie=pID;
+        RETURN name;
 	END;
 
 °°
 Delimiter °°
 °°
-CREATE PROCEDURE insertIncertaeSedisAve(
+CREATE PROCEDURE insertEspecie
 
 °°
 
 
 
+Delimiter °°
+°°
+CREATE FUNCTION getZonaVidaFromID(pID INT)   #CREADA y FUNCIONAL
+RETURNS VARCHAR(100)
+BEGIN
+	DECLARE zonaVida VARCHAR(100);
+    DECLARE CONTINUE HANDLER  FOR SQLEXCEPTION SET zonaVida='SQL_Exception';
+    SELECT Zona
+    INTO zonaVida
+    FROM zonavida
+    WHERE idZonaVida=pID;
+    RETURN zonaVida;
+END;  #¿
+
+°°
+
+DROP FUNCTION getZonaVidaFromID;
+
+SELECT idZonaVida, getZonaVidaFromID(idZonaVida)
+FROM zonavida;
+
+Delimiter °°
+°°
+
+
+°°
 Delimiter °°
 °°
 CREATE PROCEDURE insert_ave_album  (IN pUserID INT, IN pNombre VARCHAR(120), IN, pDescripcion VARCHAR(200), IN especieID VARCHAR(100), IN Canton VARCHAR(75))
