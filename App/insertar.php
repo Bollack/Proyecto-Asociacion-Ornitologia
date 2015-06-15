1e49e30be1e2c8a18812a82d072ab06d2c9982fb
Bollack <bollackdtg@gmail.com>
2015-06-14 18:42:54 -0600

    dfdf

--- App/insertar.php
+++ App/insertar.php
@@ -4,18 +4,18 @@
 	$columna = $_GET["c"];
 	$return = $_GET["r"];
 	
-	$username = "Administrador";
-	$password = "Admin13";
-	$hostname = "186.176.166.148:3306";
-	$myDB = "hidden_bird";
-	$dbhandle = mysqli_connect($hostname, $username, $password, $myDB);
-	if(!$dbhandle){
+     $username = "Administrador";
+     $password = "Admin13";
+     $hostname = "186.176.166.148:3306";
+     $myDB = "hidden_bird";
+     $dbhandle = mysqli_connect($hostname, $username, $password, $myDB);
+     if(!$dbhandle){
+     	echo "Error de conexión a la base de datos";
 		echo "Conexión fallida: " . mysqli_conect_error();
 	}else{
 		if(isset($_GET["ve"])){
 			$valorExtra =  $_GET["ve"];
 			$colExtra = "," . $_GET["ce"];
-
 			//$valorExtra = $_GET["ve"];
 			//$colExtra =  $_GET["ce"];
 			//$sql = "INSERT INTO ".$tabla." (".$columna.",".$colExtra.") VALUES (".$valor.",".$valorExtra.")";
@@ -25,6 +25,7 @@
 			$valorExtra = "";
 			$colExtra = "";
 			$sql = "INSERT INTO ".$tabla." (".$columna.") VALUES (".$valor.")";
+			echo $sql;
 		}
 		if (mysqli_query($dbhandle, $sql)) {
 			mysqli_close($dbhandle);
@@ -32,16 +33,17 @@
 			echo "setTimeout(function(){swal(\"Dato insertado\", \"Se ha insertado el dato correctamente\", \"success\");";
 		    echo "},1000);</script>";
 			sleep(1);
-			echo "<script type=\"text/javascript\">document.location.href='".$return."';</script>";
+			//echo "<script type=\"text/javascript\">document.location.href='".$return."';</script>";
 		} else {
 			$error = "Conexión fallida: " . mysqli_connect_error();
+			echo "Query cannot be made";
 			echo $error;
 		    echo "<script type=\"text/javascript\">";
 			echo "setTimeout(function(){swal(\"ERROR, PANIC!\", \"El dato no se ha podido insertar por carácteres inválidos, ya existe o por error de conexión.\"'".mysqli_connect_error()."' \"error\");";
 		    echo "},1000);;</script>";
 			sleep(1);
 			mysqli_close($dbhandle);
-			echo "<script type=\"text/javascript\">document.location.href='".$return."';</script>";		    
+			//echo "<script type=\"text/javascript\">document.location.href='".$return."';</script>";		    
 		}
 
 	}
